89a54bdcff5565986cb82934e33697cf5919e95a
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-20 13:13:53 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,26 @@
+function produceDrivingRange(blockRange) {
+  return function(a, b) {
+    let distance = parseInt(b) - parseInt(a)
+    if (distance > blockRange) {
+      return `${distance - blockRange} blocks out of range`
+    } else {
+      return `within range by ${blockRange - distance}`
+    }
+  }
+}
+
+function produceTipCalculator(tip) {
+  return function(total) {
+    return tip * total
+  }
+}
+
+function createDriver() {
+  let driverId = 0
+  return class {
+    constructor(name){
+      this.name = name
+      this.id = ++driverId
+     }
+   }
+ }
