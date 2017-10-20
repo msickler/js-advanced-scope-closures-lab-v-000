046c150c2a971495d2990b10bda93a3c2d95ba69
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-20 13:13:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,11 +16,10 @@ function produceTipCalculator(tip) {
 }
 
 function createDriver() {
-   let driverId = 0
- 
-   return class {
-     constructor(name){
-       this.name = name
+  let driverId = 0
+  return class {
+    constructor(name){
+      this.name = name
       this.id = ++driverId
      }
    }
